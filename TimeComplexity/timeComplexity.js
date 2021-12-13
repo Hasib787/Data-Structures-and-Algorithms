d1cf656167752c1936fca20f550d9621781f195f
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-13 21:43:37 +0600

    time complexity example of O(n^3)

--- TimeComplexity/timeComplexity.js
+++ TimeComplexity/timeComplexity.js
@@ -8,3 +8,17 @@ for(let i = 0; i < n; i++) {
 };
 console.log(count);
 
+let count1 = 0;
+let b = 10;
+
+for (let a = 0; a < b; a++) {
+  for (let c = 0; c < b; c++) {
+    for (let k = 0; k < b; k++) {
+      count1 = count1 + 1;
+    }
+  }
+}
+console.log("Value of count is: " + count1);
+
+
+
