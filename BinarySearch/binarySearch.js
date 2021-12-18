fa1d109b1001c0be71cd754ffc08409a341a6647
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-18 23:59:31 +0600

    learn binary search

---
+++ BinarySearch/binarySearch.js
@@ -0,0 +1,16 @@
+// Binary search
+const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) {
+        return mid;
+        } else if (arr[mid] < target) {
+        left = mid + 1;
+        } else {
+        right = mid - 1;
+        }
+    }
+    return -1;
+    }
