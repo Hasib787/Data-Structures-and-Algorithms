ddc0b130b0f4d05dd69b79eedd3d0895cf0d595c
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-19 23:52:53 +0600

    binary search example

--- BinarySearch/binarySearch.js
+++ BinarySearch/binarySearch.js
@@ -1,16 +1,19 @@
 // Binary search
-const binarySearch = (arr, target) => {
+const binarySearch = (arr, target) =>{
     let left = 0;
     let right = arr.length - 1;
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        if (arr[mid] === target) {
-        return mid;
-        } else if (arr[mid] < target) {
-        left = mid + 1;
-        } else {
-        right = mid - 1;
-        }
+    while (left <= right){
+        let mid = math.floor((left + right) / 2);
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
     }
     return -1;
-    }
+}
+const result = binarySearch([1,4,7,10,15,17,20,21,25,28,32,36,38,40,45,48,50,52,57], 10);
+console.log(result);
