49ef2010651d241c5275f1c807f9229a166456cc
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-20 00:01:34 +0600

    fix ta bug

--- BinarySearch/binarySearch.js
+++ BinarySearch/binarySearch.js
@@ -3,8 +3,8 @@ const binarySearch = (arr, target) =>{
     let left = 0;
     let right = arr.length - 1;
     while (left <= right){
-        let mid = math.floor((left + right) / 2);
-            if (arr[mid] == target) {
+        let mid = Math.floor((left + right) / 2);
+            if (arr[mid] === target) {
                 return mid;
             }
             if (arr[mid] < target) {
@@ -15,5 +15,5 @@ const binarySearch = (arr, target) =>{
     }
     return -1;
 }
-const result = binarySearch([1,4,7,10,15,17,20,21,25,28,32,36,38,40,45,48,50,52,57], 10);
+const result = binarySearch([1,4,7,10,15,17,20,21,25,28,32,36,38,40,45,48,50,52,57], 25);
 console.log(result);
