547a1be285269a698aebceb6cfda027bcd0044d0
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-22 14:36:36 +0600

    binary search example with string

--- BinarySearch/binarySearch.js
+++ BinarySearch/binarySearch.js
@@ -19,4 +19,40 @@ const targetValue = binarySearch(
   [1, 4, 7, 10, 15, 17, 20, 21, 25, 28, 32, 36, 38, 40, 45, 48, 50, 52, 57],
   25
 );
-console.log("The searching value is =",targetValue);
+console.log("The searching value is =", targetValue);
+
+var friendsName = [
+  "Alif",
+  "Arif",
+  "Bani",
+  "Bond",
+  "Cobi",
+  "Hasib",
+  "Don",
+  "Dean",
+  "Ema",
+  "Eriq",
+  "Iqbal",
+  "Jubayer",
+];
+
+const binarySearch1 = (findName) =>{
+    let leftSide = 0;
+    let rightSide = friendsName.length - 1;
+
+    while (leftSide <= rightSide){
+      let midPosition = Math.floor ((leftSide + rightSide) / 2);
+      if (friendsName[midPosition] === findName) {
+        return midPosition;
+      }
+      if (friendsName[midPosition] < findName) {
+        leftSide = midPosition + 1;
+      }else{
+        rightSide = midPosition - 1;
+      }
+    }
+    return -1;
+  } 
+
+  const findValue = binarySearch1("Hasib");
+  // console.log(findValue);
